79adaa87cd11802bd25839d977ef14210d6db438
iMprov <improv00@mail.ru>
2025-08-02 12:46:02 +0700

    Cохраняет комментарии к посту

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,9 @@ Rails.application.routes.draw do
   get 'about' => 'pages#about'
 
   resource :contacts, only: [:new, :create], path_names: {:new => ''}
-  resources :articles
+  resources :articles do
+    resources :comments
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
