f523cbcd214b7909b68141a4beaccfcb7f1a7a24
iMprov <improv00@mail.ru>
2025-08-10 13:40:39 +0700

    Тест на валидацию заголовка и описания статьи

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -2,6 +2,17 @@ require "rails_helper"
 
 describe Article do
 
-	it { should have_many :comments }
+	describe "валидация" do
+
+		it { should validate_presence_of :title }
+		it { should validate_presence_of :text }
+
+	end
+
+	describe "много к одному" do
+
+		it { should have_many :comments }
+
+	end
 
 end
